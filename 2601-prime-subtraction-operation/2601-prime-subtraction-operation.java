6a2fc0ec9a230483c33bda4c370549b0c974acc1
Vidhi Goyal <109031897+vidhi1812@users.noreply.github.com>
2024-11-11 21:47:11 +0530

    Time: 3 ms (65.79%), Space: 44.6 MB (58.28%) - LeetHub

---
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public boolean checkPrime(int x) {
+        if (x <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(x); i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean primeSubOperation(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int bound = (i == 0) ? nums[i] : nums[i] - nums[i - 1];
+            
+            if (bound <= 0) return false;
+            
+            int largestPrime = 0;
+            for (int j = bound - 1; j >= 2; j--) {
+                if (checkPrime(j)) {
+                    largestPrime = j;
+                    break;
+                }
+            }
+            
+            nums[i] -= largestPrime;
+        }
+        return true;
+    }
+}
